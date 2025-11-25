3eb93cb1ed118ba7ae33c66f2a4674f5b4f83cf6
root <root@ASUS-LLOPIS.localdomain>
2025-11-25 19:52:35 +0100

    Añadida implementación de la clase TableEntry

---
+++ TableEntry.h
@@ -0,0 +1,55 @@
+#ifndef TABLEENTRY_H
+#define TABLEENTRY_H
+
+#include <string>
+#include <ostream>
+
+template <typename V>
+class TableEntry {
+public:
+    std::string key;
+    V value;
+
+    // Constructor clave + valor
+    TableEntry(std::string key, V value) {
+        this->key = key;
+        this->value = value;
+    }
+
+    // Constructor solo clave
+    TableEntry(std::string key) {
+        this->key = key;
+    }
+
+    // Constructor por defecto
+    TableEntry() {
+        this->key = "";
+    }
+
+    // Operador ==
+    friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+        if (te1.key == te2.key) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Operador !=
+    friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+        if (te1.key != te2.key) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Operador << para imprimir ('key' => value)
+    friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te) {
+        out << "('" << te.key << "' => " << te.value << ")";
+        return out;
+    }
+};      
+ 
+#endif
+
